a3a95f9781e1a8591bb9e318fb6d1afb84d2634a
HarshitaKachhadiya <kachhadiyaharshi456@gmail.com>
2023-02-25 08:11:51 +0530

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#00000099',
+        secondary: '#00000033'
+      }
+    },
   },
   plugins: [],
 }
